29f4395d810b6f5378246b297a60e1173a653209
HappyClown <gaskin.xavier@gmail.com>
2018-01-23 15:30:54 -0500

    Park kite string puzzle progress

---
+++ Assets/Scripts/Park/ClickToRotateTile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToRotateTile : MonoBehaviour 
+{
+
+	Ray2D ray;
+	RaycastHit2D hit;
+	Vector2 mousePos2D;
+	Vector3 mousePos;
+
+
+	
+	void FixedUpdate () 
+	{
+		mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePos2D = new Vector2 (mousePos.x, mousePos.y);
+
+		hit = Physics2D.Raycast(mousePos2D, Vector3.forward, 50f);
+	}
+
+
+	void Update()
+	{
+		if (hit.collider != null && Input.GetMouseButtonDown(0) && hit.collider.CompareTag("Tile"))
+		{
+			Debug.Log(hit.collider.name);
+			hit.collider.transform.eulerAngles = new Vector3(hit.collider.transform.eulerAngles.x, hit.collider.transform.eulerAngles.y, hit.collider.transform.eulerAngles.z - 90);
+		}
+	}
+}
---
+++ Assets/Scripts/Park/TileRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileRotation : MonoBehaviour 
+{
+
+
+	public int initialRot;
+
+	public float zRotation;
+
+
+	void Start () 
+	{
+		zRotation = this.transform.rotation.z;
+
+		initialRot = Random.Range(0,4);
+
+		if (initialRot == 0) { zRotation = 0; } else if (initialRot == 1) { zRotation = 90; } else if (initialRot == 2) { zRotation = 180; } else if (initialRot == 3) { zRotation = 270; }
+
+		this.transform.eulerAngles = new Vector3(this.transform.rotation.x, this.transform.rotation.y, zRotation);
+
+	}
+
+}
