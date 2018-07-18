0299313ad1e171a2d377a338d20df7e1a3bfef84
Unknown <camilocastro316@gmail.com>
2018-07-17 21:12:29 -0400

    C - Cafe Puzzle first set up... yay its me here!!

---
+++ Assets/Scripts/Cafe/CafePuzzleCell.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CafePuzzleCell : MonoBehaviour {
+
+	//We have to asing by hand the cells next to it, if there is no cell we leave it null
+	public CafePuzzleCell cellUp;
+	public CafePuzzleCell cellDown;
+	public CafePuzzleCell cellLeft;
+	public CafePuzzleCell cellRight;
+	//the bools define if the cell is next to an edge of the board
+	public bool edgeUp, edgeDown, edgeLeft, edgeRight;
+	//the ovvupied bool defines if the cell has something on it
+	public bool occupied;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//when the player moves a cup, the cells will check by functions if its cells next to it are occupied
+	public CafePuzzleCell CheckUp(){
+		//The cell returns itself if the cell next to it in the selected direction is occupied or if there is an edge 
+		if(edgeUp){
+			return this;
+		}
+		else if(cellUp.occupied){
+			return this;
+		}
+		else{
+		//if not, the next cell will repeat the process. the same script applies for each direction.
+			return cellUp.CheckUp();
+		}
+	}
+	public CafePuzzleCell CheckDown(){
+		if(edgeDown){
+			return this;
+		}
+		else if(cellDown.occupied){
+			return this;
+		}
+		else{
+			return cellDown.CheckDown();
+		}
+	}
+	public CafePuzzleCell CheckLeft(){
+		if(edgeLeft){
+			return this;
+		}
+		else if(cellLeft.occupied){
+			return this;
+		}
+		else{
+			return cellLeft.CheckLeft();
+		}
+	}
+	public CafePuzzleCell CheckRight(){
+		if(edgeRight){
+			return this;
+		}
+		else if(cellRight.occupied){
+			return this;
+		}
+		else{
+			return cellRight.CheckRight();
+		}
+	}
+}
---
+++ Assets/Scripts/Cafe/CafePuzzleCup.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CafePuzzleCup : MonoBehaviour {
+
+	public CafePuzzleCell startingCell,curretCell, nextCell;
+	public bool moving;
+	public float animationSpeed;
+	public enum cupColor{
+		Red,
+		blue,
+		green
+	}
+	public cupColor myColor;
+	private float remainingDist, distToPoint;
+	private Vector2 nextPos,currentPos;
+	public AnimationCurve moveAnimation;
+	// Use this for initialization
+	void Start () {
+		Vector2 startingPos = new Vector3(startingCell.gameObject.transform.position.x, startingCell.transform.position.y);
+		transform.position = startingPos;
+		startingCell.occupied = true;
+		curretCell = startingCell;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!moving){
+			if(Input.GetKey(KeyCode.UpArrow)){
+				nextCell = curretCell.CheckUp();
+				nextCell.occupied = true;
+				nextPos = nextCell.gameObject.transform.position;
+				distToPoint = Vector2.Distance(transform.position,nextPos);
+				moving = true;
+			}
+			else if(Input.GetKey(KeyCode.DownArrow)){
+				nextCell = curretCell.CheckDown();
+				nextCell.occupied = true;
+				nextPos = nextCell.gameObject.transform.position;
+				distToPoint = Vector2.Distance(transform.position,nextPos);
+				moving = true;			
+			}
+			else if(Input.GetKey(KeyCode.LeftArrow)){
+				nextCell = curretCell.CheckLeft();
+				nextCell.occupied = true;
+				nextPos = nextCell.gameObject.transform.position;
+				distToPoint = Vector2.Distance(transform.position,nextPos);
+				moving = true;			
+			}
+			else if(Input.GetKey(KeyCode.RightArrow)){
+				nextCell = curretCell.CheckRight();
+				nextCell.occupied = true;
+				nextPos = nextCell.gameObject.transform.position;
+				distToPoint = Vector2.Distance(transform.position,nextPos);
+				moving = true;			
+			}
+		}
+		else{
+			if(Vector2.Distance(transform.position,nextPos) <  0.05f){
+				transform.position = nextPos;
+				moving = false;
+				curretCell.occupied = false;
+				curretCell = nextCell;
+			}
+			else{
+				transform.position = Vector2.MoveTowards(transform.position,nextPos,Time.deltaTime*(moveAnimation.Evaluate(Vector2.Distance(transform.position,nextPos)/distToPoint))*animationSpeed);
+			}
+		}
+	}
+}
