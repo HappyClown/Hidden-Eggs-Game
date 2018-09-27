27f13e1c2399087df6caa11c2371894b36fa0940
Unknown <camilocastro316@gmail.com>
2018-09-26 23:36:23 -0400

    C - Double tap and panning camera!!

--- Assets/Scripts/_General/LevelTapMannager.cs
+++ Assets/Scripts/_General/LevelTapMannager.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PinchZoom : MonoBehaviour 
-{	
+public class LevelTapMannager : MonoBehaviour {
+
 	[Header("Main Camera in the level")]
 	public Camera cam;
-	[Header("Camera movement speed (usually is 5)")]
+	//Tap and panning variables
+	//Click on egg script reference for the single tap
+	public ClickOnEggs myClickOnEggs;
+	//counter to know the amount of taps, and time between taps, and tap time
+	private float tapCounter = 0 , tapTimer = 0, doubleTapTimer = 0;
+	//Death zone for panning move and min time for double tap and the speed
+	[Header("5 - 10 - 0.5 - 3 - 5")]
+	public float panningDeathZone, panningSpeed, minDoubleTapTime, dTapMoveSpeed, dTapZoomSpeed;
+	//Panning Move bool nad doubleTapped
+	private bool panningNove, doubleTapped, zoomIn;
+	//movement vectors
+	private Vector2 lastPosition;
+	//Zoom Variables
+	[Header("Camera movement speed (usually is 2)")]
 	public float camMovSpeed;
-	[Header("Camera zoom speed (usually is 5)")]
+	[Header("Camera zoom speed (usually is 50)")]
 	public float camZoomSpeed;
 	//touch position holders
 	private Vector2 centerPoint, touchZeroPos, touchOnePos;
@@ -18,7 +31,7 @@ public class PinchZoom : MonoBehaviour
 	private bool touching = false, zooming = false;
 	//touch variables holders
 	private Touch touchZero, touchOne;
-	[Header("Camera Min Zoom Size (orthographicSize usually 5)")]
+	[Header("Camera Min Ortho Size (usually 5)")]
 	public float minCameraSize;
 	//max camera orthographicsize is taken from the camera
 	private float maxCameraSize;
@@ -40,18 +53,58 @@ public class PinchZoom : MonoBehaviour
 		currentCameraSize = cam.orthographicSize;
 		//get the initial camera X and Y values
 		initialCameraPosition = cam.transform.position;
+		//panning move initialization in false
+		panningNove = false;
 	}
 	void Update ()
 	{
+		if(doubleTapped){
+			DoubleTap();
+		}
+		else if (Input.touchCount == 1){
+			touchZero = Input.GetTouch(0);
+			if(touchZero.phase == TouchPhase.Began){
+				tapCounter ++;
+				touchZeroPos = touchZero.position;
+				lastPosition = touchZeroPos;
+			}
+			if(Vector2.Distance(touchZeroPos,touchZero.position) > panningDeathZone){
+				panningNove = true;
+			}
+			if(touchZero.deltaTime > minDoubleTapTime && touchZero.phase == TouchPhase.Ended && !panningNove){				
+				DetectEggs();
+				tapCounter = 0;
+			}			
+			else if(tapCounter == 2 && doubleTapTimer < minDoubleTapTime){
+				DoubleTap();
+				tapCounter = 0;
+				doubleTapTimer = 0;
+			}else if(panningNove){
+				PanningCamera();
+			}
+		}
 		//check if the player is doing two contacts
-		if (Input.touchCount == 2) { 
+		else if (Input.touchCount == 2) { 
+			tapCounter = 0;
+			panningNove = false;
 			//activate the zoom function
 			PinchCamZoom(); 
 		}else{
 			//reset values when the player is not doing two contacts
 			zooming = false;
 			currentDeltaDif = 0;
-		}		
+			panningNove = false;
+		}
+		if(tapCounter == 1){
+			doubleTapTimer += Time.deltaTime;
+			if(doubleTapTimer > minDoubleTapTime && !panningNove){
+				DetectEggs();
+				tapCounter = 0;
+				doubleTapTimer = 0;
+			}
+		}
+		
+		FixCameraPosition();		
 	}
 
 	void PinchCamZoom()
@@ -94,7 +147,7 @@ public class PinchZoom : MonoBehaviour
 			{
 				//if the previus delta is bigger than the current delta, it means the player is closing the fingers, so we zoom out
 				//if(prevTouchDeltaMag > touchDeltaMag){					
-					cam.transform.position = Vector3.Lerp(cam.transform.position,initialCameraPosition, Time.deltaTime * camMovSpeed);
+					//cam.transform.position = Vector3.Lerp(cam.transform.position,initialCameraPosition, Time.deltaTime * camMovSpeed);
 					//cam.transform.Translate((initialCameraPosition - new Vector2(cam.transform.position.x, cam.transform.position.y)).normalized * (camMovMoveSpeed * Vector2.Distance(centerPoint,cam.transform.position)), Space.World);
 				//}
 				//if the previus delta is less than the current delta, it means the player is oppening the fingers, so we zoom in
@@ -109,17 +162,56 @@ public class PinchZoom : MonoBehaviour
 				//clamp the value in between the camera limits
 				cam.orthographicSize = Mathf.Clamp(currentCameraSize, minCameraSize, maxCameraSize);
 				//update the current camera size
-				currentCameraSize = cam.orthographicSize;
-
-				currentX = (maxX*(currentCameraSize-maxCameraSize)*-1)/(maxCameraSize - minCameraSize);
-				currentY = (maxY*(currentCameraSize-maxCameraSize)*-1)/(maxCameraSize - minCameraSize);	
-				
-				if(cam.transform.position.x > currentX){cam.transform.position = new Vector3(currentX,cam.transform.position.y,initialCameraPosition.z);}
-				if(cam.transform.position.x < (currentX*-1)){cam.transform.position = new Vector3((currentX*-1),cam.transform.position.y,initialCameraPosition.z);}
-				if(cam.transform.position.y > currentY){cam.transform.position = new Vector3(cam.transform.position.x,currentY,initialCameraPosition.z);}
-				if(cam.transform.position.y < (currentY*-1)){cam.transform.position = new Vector3(cam.transform.position.x,(currentY*-1),initialCameraPosition.z);}
-				
+				currentCameraSize = cam.orthographicSize;				
 			}
 	}
-	
+
+	void DetectEggs(){
+		Debug.Log("detecting eggs!!!");
+	}
+	void DoubleTap(){
+		Debug.Log("doube tap!!!");
+		if(!doubleTapped){
+			doubleTapped = true;
+			centerPoint =  cam.ScreenToWorldPoint(touchZero.position);
+			if(cam.orthographicSize < maxCameraSize - 0.1){
+				zoomIn = false;
+			}
+			else{
+				zoomIn = true;
+			}
+		}
+		if(zoomIn){
+			cam.transform.position = Vector3.Lerp(cam.transform.position,new Vector3(centerPoint.x,centerPoint.y,initialCameraPosition.z), Time.deltaTime * dTapMoveSpeed);
+			currentCameraSize = Mathf.Lerp(currentCameraSize,minCameraSize,Time.deltaTime * dTapZoomSpeed);
+			if(currentCameraSize <= minCameraSize + 0.01){
+				doubleTapped = false;
+			}
+		}
+		else{
+			cam.transform.position = Vector3.Lerp(cam.transform.position,initialCameraPosition, Time.deltaTime * dTapMoveSpeed);
+			currentCameraSize = Mathf.Lerp(currentCameraSize,maxCameraSize,Time.deltaTime * dTapZoomSpeed);
+			if(currentCameraSize >= maxCameraSize - 0.01){
+				doubleTapped = false;
+			}
+		}
+		cam.orthographicSize = Mathf.Clamp(currentCameraSize, minCameraSize, maxCameraSize);
+
+	}
+	void PanningCamera(){
+		if(touchZero.phase == TouchPhase.Moved && cam.orthographicSize < maxCameraSize){
+		Vector2 dir = (touchZero.position - lastPosition).normalized * -1;
+		cam.transform.position += new Vector3(dir.x,dir.y,0)*Time.deltaTime*panningSpeed;
+		lastPosition = touchZero.position;
+		}
+	}
+	void FixCameraPosition(){
+		currentX = (maxX*(currentCameraSize-maxCameraSize)*-1)/(maxCameraSize - minCameraSize);
+		currentY = (maxY*(currentCameraSize-maxCameraSize)*-1)/(maxCameraSize - minCameraSize);	
+				
+		if(cam.transform.position.x > currentX){cam.transform.position = new Vector3(currentX,cam.transform.position.y,initialCameraPosition.z);}
+		if(cam.transform.position.x < (currentX*-1)){cam.transform.position = new Vector3((currentX*-1),cam.transform.position.y,initialCameraPosition.z);}
+		if(cam.transform.position.y > currentY){cam.transform.position = new Vector3(cam.transform.position.x,currentY,initialCameraPosition.z);}
+		if(cam.transform.position.y < (currentY*-1)){cam.transform.position = new Vector3(cam.transform.position.x,(currentY*-1),initialCameraPosition.z);}
+	}
 }
