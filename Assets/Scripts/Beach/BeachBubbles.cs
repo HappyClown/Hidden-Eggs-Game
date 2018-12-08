91f93fd027e3b3a67c52ce7de10e59c826dadd32
Unknown <camilocastro316@gmail.com>
2018-12-08 12:25:53 -0500

    C - Clam puzzle bubbles!

---
+++ Assets/Scripts/Beach/BeachBubbles.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeachBubbles : MonoBehaviour {
+
+	[Range(0.1f,1f)]
+	public float bubbleSize = 1f;
+	public float Speed;
+	public AnimationCurve curveTrail;
+	[Range(1f,10f)]
+	public float curveMultiplier = 1;
+	public FadeInOutSprite myFade;
+	public float lifeTime;
+	public float lifeTimedelay;
+	public bool activeClam;
+	private bool activeSprite;
+	private bool fadeInOutSprite;
+	private float currentTime;
+	private Vector3 StartPosition;
+	private SpriteRenderer mySprite;
+	// Use this for initialization
+	void Start () {
+		activeClam = false;
+		gameObject.transform.localScale = gameObject.transform.localScale * (bubbleSize);
+		StartPosition = gameObject.transform.localPosition;
+		currentTime = 0;
+		mySprite = this.gameObject.GetComponent<SpriteRenderer>();
+		myFade = this.gameObject.GetComponent<FadeInOutSprite>();
+		mySprite.enabled = false;
+		activeSprite = false;
+		fadeInOutSprite = false;
+		myFade.FadeOut();	
+	}
+	// Update is called once per frame
+	void Update () {
+		if(activeClam){
+			if(!activeSprite && currentTime > lifeTimedelay){
+				mySprite.enabled = true;
+				activeSprite = true;
+				myFade.FadeIn();
+			}
+			currentTime += Time.deltaTime;
+			if(currentTime < (lifeTime+lifeTimedelay) && currentTime > lifeTimedelay){
+				float yPos = gameObject.transform.localPosition.y + (Time.deltaTime*Speed);
+				float xPos = curveTrail.Evaluate(Time.time)*curveMultiplier;
+				Vector3 newPos = new Vector3(xPos,yPos,gameObject.transform.localPosition.z);
+				gameObject.transform.localPosition = newPos;
+			}
+			else{
+				if(!fadeInOutSprite){
+					myFade.FadeOut();
+					fadeInOutSprite = true;
+				}
+			}
+		}
+		else{
+			currentTime = 0;
+			mySprite.enabled = false;
+			gameObject.transform.localPosition =StartPosition;
+			activeSprite = false;
+		}
+	}
+}
