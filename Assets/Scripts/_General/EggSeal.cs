24aefb20efc3c21854c3f9dd68b98bcbbed252e3
HappyClown <gaskin.xavier@gmail.com>
2018-12-20 22:52:47 -0500

    X- Egg Seal anim + FX test

---
+++ Assets/Scripts/_General/EggSeal.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggSeal : MonoBehaviour {
+	public Animator anim;
+
+	void PlayEggSeal() {
+		anim.SetTrigger("PlaySeal");
+	}
+}
