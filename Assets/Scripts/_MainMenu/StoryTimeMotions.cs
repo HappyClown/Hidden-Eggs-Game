a646e1caa3e1f610f02d8c93136174bc696815b8
HappyClown <gaskin.xavier@gmail.com>
2019-03-11 18:04:12 -0400

    X- Story...

--- Assets/Scripts/_MainMenu/StoryTimeMotions.cs
+++ Assets/Scripts/_MainMenu/StoryTimeMotions.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class StoryTimeMotions : MonoBehaviour {
 	public GameObject normalTime;
-	[Header("")]
+	[Header("Move In")]
+	public Transform endTrans;
 	public bool timeMovesIn;
 	private float lerpValue;
-	public Transform endTrans;
+	public AnimationCurve moveInAnimCurve;
+	public AnimationCurve scaleInAnimCurve;
 	private Vector3 startPos;
 	public float endScale;
 	private float newScale, startScale;
 	public float moveInDuration;
-	public AnimationCurve moveInAnimCurve;
-	public AnimationCurve scaleInAnimCurve;
+	[Header("Hover")]
+	public float hoverDuration;
+	private float hoverLerpValue;
+	public bool hoverUp, hoverDown;
+	public Transform topYTrans, botYTrans;
+	private float topY, botY;
+	private float newXMagnitude;
+	public float newXMagMin, newXMagMax;
+	public AnimationCurve hoverYAnimCurve;
+	public AnimationCurve hoverXAnimCurve;
 	public FadeInOutSprite normTimeFadeScript;
 
 	void Start () {
@@ -31,6 +41,44 @@ public class StoryTimeMotions : MonoBehaviour {
 
 			newScale = Mathf.Lerp(startScale, endScale, scaleInAnimCurve.Evaluate(lerpValue));
 			normalTime.transform.localScale = new Vector3(newScale, newScale, newScale);
+
+			if (lerpValue >= 1f) {
+				hoverUp = true;
+				botY = normalTime.transform.localPosition.y;
+				topY = topYTrans.localPosition.y;
+				newXMagnitude = Random.Range(newXMagMin, newXMagMax);
+			}
+			if (lerpValue >= 1) {
+				lerpValue = 0;
+				timeMovesIn = false;
+			}
+		}
+		if (hoverUp) {
+			hoverLerpValue += Time.deltaTime / hoverDuration;
+			float newY = Mathf.Lerp(botY, topY, hoverYAnimCurve.Evaluate(hoverLerpValue));
+			float newX = hoverXAnimCurve.Evaluate(hoverLerpValue) * newXMagnitude;
+
+			normalTime.transform.position = new Vector3(endTrans.transform.position.x + newX, newY, normalTime.transform.position.z);
+			if(hoverLerpValue >= 1) {
+				hoverUp = false;
+				hoverDown = true;
+				hoverLerpValue = 0;
+				botY = botYTrans.localPosition.y;
+				newXMagnitude = Random.Range(newXMagMin, newXMagMax);
+			}
+		}
+		else if (hoverDown) {
+			hoverLerpValue += Time.deltaTime / hoverDuration;
+			float newY = Mathf.Lerp(topY, botY, hoverYAnimCurve.Evaluate(hoverLerpValue));
+			float newX = hoverXAnimCurve.Evaluate(hoverLerpValue) * -newXMagnitude;
+			
+			normalTime.transform.position = new Vector3(endTrans.transform.position.x + newX, newY, normalTime.transform.position.z);
+			if(hoverLerpValue >= 1) {
+				hoverUp = true;
+				hoverDown = false;
+				hoverLerpValue = 0;
+				newXMagnitude = Random.Range(newXMagMin, newXMagMax);
+			}
 		}
 	}
 }
