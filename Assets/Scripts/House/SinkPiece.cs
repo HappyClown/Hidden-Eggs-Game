44ef481a7b14e7069afab359dfdea782a4be2729
Camilo <camilocastro316@gmail.com>
2019-03-11 22:17:46 -0400

    C - House Puzzle Set up 2

---
+++ Assets/Scripts/House/SinkPiece.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkPiece : MonoBehaviour {
+	 public enum pieceTypes
+	 {
+		 dish,
+		 soap,
+		 sponge,
+		 bubble
+	 }
+	public pieceTypes pieceType;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
