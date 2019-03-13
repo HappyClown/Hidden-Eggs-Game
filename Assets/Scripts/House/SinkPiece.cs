a3967f7f54f0e6c95765622ff4fd458a1b2f78e1
Camilo <camilocastro316@gmail.com>
2019-03-12 22:35:36 -0400

    C - House Puzzle Falling

--- Assets/Scripts/House/SinkPiece.cs
+++ Assets/Scripts/House/SinkPiece.cs
@@ -11,13 +11,25 @@ public class SinkPiece : MonoBehaviour {
 		 bubble
 	 }
 	public pieceTypes pieceType;
+	public PuzzleCell StartingCell, currentCell, nextCell;
+	public bool selected, falling;
+	public float minDistance, gravity, speed;
 	// Use this for initialization
 	void Start () {
-		
+		currentCell = StartingCell;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(falling){
+			if(this.transform.position.y < (currentCell.gameObject.transform.position.y + minDistance)){
+				falling = false;
+				speed = 0;
+			}
+			else{
+				speed += gravity*Time.deltaTime;
+				this.transform.position = Vector2.MoveTowards(this.transform.position,currentCell.gameObject.transform.position,speed);
+			}
+		}
 	}
 }
