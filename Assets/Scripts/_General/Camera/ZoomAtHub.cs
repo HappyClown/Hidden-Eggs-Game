119f22a8aef1a0f373ddebebe1ea70a5de1d28f8
HappyClown <gaskin.xavier@gmail.com>
2019-03-28 21:54:10 -0400

    X&C- Cam zoom

--- Assets/Scripts/_General/Camera/ZoomAtHub.cs
+++ Assets/Scripts/_General/Camera/ZoomAtHub.cs
@@ -6,8 +6,8 @@ public class ZoomAtHub : MonoBehaviour {
 
 	private bool zoomIn;
 	public float fromZoom, toZoom, speed, zoomSpeed;
-	private float currentZoom;
-	private Vector3 ZoomPosition;
+	private float currentZoom, lerpValue, camLerpValue;
+	private Vector3 zoomPosition, startPosition;
 	public Camera myCam;
 	// Use this for initialization
 	void Start () {
@@ -18,13 +18,16 @@ public class ZoomAtHub : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(zoomIn){
-			myCam.gameObject.transform.position = Vector3.Lerp(myCam.gameObject.transform.position,ZoomPosition, Time.deltaTime * speed);
-			currentZoom = myCam.orthographicSize;
-			myCam.orthographicSize = Mathf.Lerp(currentZoom,toZoom,Time.deltaTime * zoomSpeed);
+			lerpValue += Time.deltaTime * speed;
+			myCam.gameObject.transform.position = Vector3.Lerp(startPosition, zoomPosition, lerpValue);
+			camLerpValue += Time.deltaTime * zoomSpeed;
+			myCam.orthographicSize = Mathf.Lerp(currentZoom, toZoom, camLerpValue);
 		}
 	}
 	public void StartZoom(Vector3 toPosition){
 		zoomIn = true;
-		ZoomPosition = toPosition;
+		zoomPosition = toPosition;
+		startPosition = myCam.transform.position;
+		currentZoom = myCam.orthographicSize;
 	}
 }
