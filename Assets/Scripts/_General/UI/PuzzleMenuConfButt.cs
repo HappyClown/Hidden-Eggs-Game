bf59cb80f0c58ccada3833038d8d2aaa83676e75
Camilo <camilocastro316@gmail.com>
2019-06-20 23:10:14 -0400

    C - Puzzle Confirmation Done!

---
+++ Assets/Scripts/_General/UI/PuzzleMenuConfButt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleMenuConfButt : MonoBehaviour {
+
+	public ClickOnEggs myClickOnEggs;
+	public Button button;
+
+	void Start () {
+		button = this.GetComponent<Button>();
+		button.onClick.AddListener(OpenPuzzle);
+	}
+	
+	void OpenPuzzle () {
+		myClickOnEggs.LoadPuzzle();
+	}
+}
