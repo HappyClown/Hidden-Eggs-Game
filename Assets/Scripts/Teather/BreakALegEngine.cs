4ab62607f4a5ff1ae28286019e80ba43a93a3e96
Camilo <camilocastro316@gmail.com>
2019-07-04 23:17:16 -0400

    C - BreakALeg Prototype!

---
+++ Assets/Scripts/Teather/BreakALegEngine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakALegEngine : MonoBehaviour {
+
+	public BreakALegLvl[] mylvls;
+	public int currentLvl;
+	// Use this for initialization
+	void Start () {
+		currentLvl = 0;
+		mylvls[currentLvl].SetUpLvl();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Q)){
+			mylvls[currentLvl].MoveBag1(true);
+		}else if(Input.GetKeyDown(KeyCode.A)){
+			mylvls[currentLvl].MoveBag1(false);
+		}else if(Input.GetKeyDown(KeyCode.W)){
+			mylvls[currentLvl].MoveBag2(true);
+		}else if(Input.GetKeyDown(KeyCode.S)){
+			mylvls[currentLvl].MoveBag2(false);
+		}else if(Input.GetKeyDown(KeyCode.E)){
+			mylvls[currentLvl].MoveBag3(true);
+		}else if(Input.GetKeyDown(KeyCode.D)){
+			mylvls[currentLvl].MoveBag3(false);
+
+		}
+	}
+}
---
+++ Assets/Scripts/Teather/TeatherBag.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeatherBag : MonoBehaviour {
+
+	public float Value;
+	public bool up, down;
+	public int movesQuantity;
+	private int currentMove;
+	public float amountToMove;
+	// Use this for initialization
+	void Start () {
+		currentMove = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void MoveBagUp(){
+		currentMove ++;
+		if(down){
+			down = false;
+		}
+		else if(Mathf.Abs(currentMove) == movesQuantity){
+			up = true;
+		}
+		Vector3 currentPos = this.gameObject.GetComponent<RectTransform>().position;
+
+		this.gameObject.GetComponent<RectTransform>().position = new Vector3(currentPos.x, currentPos.y+amountToMove,currentPos.z);
+	}
+	public void MoveBagDown(){
+		currentMove --;
+		if(up){
+			up = false;
+		}
+		else if(Mathf.Abs(currentMove) == movesQuantity){
+			down = true;
+		}
+		Vector3 currentPos = this.gameObject.GetComponent<RectTransform>().position;
+
+		this.gameObject.GetComponent<RectTransform>().position = new Vector3(currentPos.x, currentPos.y-amountToMove,currentPos.z);
+	}
+}
