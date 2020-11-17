f494f3bc9c07b4a57a55714fb80d6349947fa182
Camilo <camilocastro316@gmail.com>
2020-11-17 17:09:12 -0500

    C - Toy Store Puzzle Set up

---
+++ Assets/Scripts/ToyStore/ToyStorePuzzleLevel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyStorePuzzleLevel : MonoBehaviour {
+	public bool levelComplete, finished;
+	public float snapRadius, backDuration;
+	public ToyStorePuzzlePiece[] myPieces;
+	public PuzzleCell[] gridCells;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void SetUpLevel(){
+		levelComplete = false;
+		finished = false;
+	}
+	public void ResetLevel(){
+
+	}
+	public void FreeCells(ToyStorePuzzlePiece piece){
+		foreach (PuzzleCell pieceCell in piece.mycells)
+			{	
+				//set logic here
+			}
+	}
+}
